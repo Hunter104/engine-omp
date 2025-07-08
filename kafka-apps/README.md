a3f812971eb7dd85b77df55cdf48501d4de787b8
Hunter104 <000plagueinc@gmail.com>
2025-07-08 16:58:47 -0300

    Adiciona configuração externa

--- kafka-apps/README.md
+++ kafka-apps/README.md
@@ -14,6 +14,14 @@ mvn clean install
 
 Obs: precisa ter o maven instalado
 
+## Configuração
+A prioridade de configuração é, em ordem:
+
+1. Propriedades de sistema/jvm
+2. Variáveis de ambiente
+3. config.properties do módulo commons
+
+na raíz do projeto há um arquivo .env.example para demonstração de configuração
 ## Como executar
 
 Certifique-se de que o Kafka está rodando.
--- kafka-apps/common/src/main/java/com/pspd/ConfigLoader.java
+++ kafka-apps/common/src/main/java/com/pspd/ConfigLoader.java
@@ -1,36 +1,33 @@
 package com.pspd;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import org.apache.commons.configuration2.*;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-// TODO: mover para configuração externa
+import java.io.File;
+
 public class ConfigLoader {
-    private static final Properties properties = new Properties();
+    private static final Logger logger = LoggerFactory.getLogger(ConfigLoader.class);
+    private static final CompositeConfiguration configs = new CompositeConfiguration();
 
     static {
-        try (InputStream is = ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties")) {
-            if (is == null) {
-                throw new RuntimeException("config.properties not found");
-            }
-            properties.load(is);
-        } catch (IOException e) {
-            throw new RuntimeException("Error loading config.properties: " + e.getMessage());
+        Configurations helper = new Configurations();
+        configs.setThrowExceptionOnMissing(true);
+        try {
+            configs.addConfiguration(new SystemConfiguration());
+            configs.addConfiguration(new EnvironmentConfiguration());
+            configs.addConfiguration(helper.properties(new File("config.properties")));
+
+            logger.info("Configuration loaded succesfully");
+        } catch (ConfigurationException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private static String getProperty(String key) {
-        String value = properties.getProperty(key);
-        if (value == null || value.trim().isEmpty()) {
-            throw new RuntimeException("Required property '" + key + "' not found or empty in config.properties");
-        }
-
-        return value;
-    }
-
-    public static final String BOOTSTRAP_SERVERS = getProperty("kafka.bootstrap.servers");
-    public static final String GROUP_ID = getProperty("kafka.group.id");
-    public static final String TOPIC = getProperty("kafka.topic");
-    public static final String EXECUTABLE = getProperty("app.executable");
-    public static final String MAX_POLLING_TIMEOUT = getProperty("kafka.max.poll.interval.ms");
+    public static final String BOOTSTRAP_SERVERS = configs.getString("kafka.bootstrap.servers");
+    public static final String GROUP_ID = configs.getString("kafka.group.id");
+    public static final String TOPIC = configs.getString("kafka.topic");
+    public static final String EXECUTABLE = configs.getString("app.executable");
 }
