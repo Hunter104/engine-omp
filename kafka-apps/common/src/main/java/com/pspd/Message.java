b0b5821fcc1f65c34d13781d88e9d3a5da4a47a7
Hunter104 <000plagueinc@gmail.com>
2025-07-08 14:19:42 -0300

    Restrição de tabuleiro de tamanho 1

--- kafka-apps/common/src/main/java/com/pspd/Message.java
+++ kafka-apps/common/src/main/java/com/pspd/Message.java
@@ -3,8 +3,8 @@ package com.pspd;
 
 public record Message(int powMin, int powMax) {
     public Message {
-        if (powMin < 0 || powMax < 0) {
-            throw new IllegalArgumentException("Power must be positive");
+        if (powMin <= 1 || powMax <= 1) {
+            throw new IllegalArgumentException("Power must be greater than one");
         }
         if (powMin > powMax) {
             throw new IllegalArgumentException("powMin must be less than powMax");
