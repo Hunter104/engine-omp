0a95e6df46c891479e534c916d1017ff6da40c59
Hunter104 <000plagueinc@gmail.com>
2025-07-09 15:12:54 -0300

    Move logs para arquivo

--- kafka-apps/kafka-producer/src/main/java/com/pspd/ProducerApp.java
+++ kafka-apps/kafka-producer/src/main/java/com/pspd/ProducerApp.java
@@ -2,11 +2,15 @@ package com.pspd;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 import java.util.Scanner;
 
 public class ProducerApp {
+    private static final Logger logger = LoggerFactory.getLogger(ProducerApp.class);
+
     public static void main(String[] args) {
         Properties props = ConfigLoader.getProducerProps();
         System.out.println("Producer started, enter min and max values separated by a space (quit with exit):");
@@ -23,10 +27,13 @@ public class ProducerApp {
                 producer.send(new ProducerRecord<>(props.getProperty("app.topic"), gameOfLifeArgs));
             }
         } catch (NumberFormatException e) {
+            logger.error("Invalid input: {}", e.getMessage());
             System.err.println("Invalid input");
         } catch (IllegalArgumentException e) {
+            logger.error("Error creating message {}", e.getMessage());
             System.err.println("Error while creating message: " + e.getMessage());
         } catch (Exception e) {
+            logger.error("Error sending message: {}", e.getMessage());
             System.err.println("Error sending message: " + e.getMessage());
         }
         System.out.println("Producer stopped");
