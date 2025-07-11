77f000fe42713fab56340d94fd99d4fcb46956f3
Hunter104 <000plagueinc@gmail.com>
2025-07-11 14:46:37 -0300

    Conserta config kafka não carregar

--- kafka-apps/common/src/main/java/com/pspd/ConfigLoader.java
+++ kafka-apps/common/src/main/java/com/pspd/ConfigLoader.java
@@ -4,65 +4,72 @@ import org.apache.commons.configuration2.*;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.tree.OverrideCombiner;
+import org.apache.commons.lang3.NotImplementedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public class ConfigLoader {
     private static final Logger logger = LoggerFactory.getLogger(ConfigLoader.class);
-    private static final CombinedConfiguration combinedConfig;
+    private static final CombinedConfiguration appConfig;
     private static final PropertiesConfiguration kafkaConfig;
+    private static final String MAIN_CONFIG_FILE = "app.properties";
+    private static final String KAFKA_CONFIG_FILE = "kafka.properties";
 
     static {
         try {
-            combinedConfig = new CombinedConfiguration(new OverrideCombiner());
+            appConfig = new CombinedConfiguration(new OverrideCombiner());
             Configurations configs = new Configurations();
-            combinedConfig.setThrowExceptionOnMissing(true);
+            appConfig.setThrowExceptionOnMissing(true);
 
-            kafkaConfig = configs.properties(
-                    ConfigLoader.class.getClassLoader().getResource("kafka.properties")
-            );
-            logger.info("Loaded kafka config from classpath");
+            kafkaConfig = configs.properties(ConfigLoader.class.getClassLoader().getResource(KAFKA_CONFIG_FILE));
+            kafkaConfig.setThrowExceptionOnMissing(true);
+            logger.info("Loaded kafka config from classpath {}", kafkaConfig);
 
 
-            File externalFile = new File("app.properties");
+            File externalFile = new File(MAIN_CONFIG_FILE);
             if (externalFile.exists()) {
-                combinedConfig.addConfiguration(configs.properties(externalFile));
+                appConfig.addConfiguration(configs.properties(externalFile));
                 logger.info("Loaded external config from {}", externalFile.getAbsolutePath());
             } else {
                 logger.warn("External config file not found: {}", externalFile.getAbsolutePath());
             }
 
             PropertiesConfiguration defaultConfig = configs.properties(
-                    ConfigLoader.class.getClassLoader().getResource("app.properties")
+                    ConfigLoader.class.getClassLoader().getResource(MAIN_CONFIG_FILE)
             );
             logger.info("Loaded default config from classpath");
-            combinedConfig.addConfiguration(defaultConfig);
+            appConfig.addConfiguration(defaultConfig);
 
         } catch (ConfigurationException e) {
             throw new RuntimeException("Failed to load config", e);
         }
     }
 
-    public static final String EXECUTABLE = combinedConfig.getString("app.executable");
-    public static final String ENVIRONMENT = combinedConfig.getString("app.environment", "local");
-    public static final String IMAGE = combinedConfig.getString("app.image");
-    public static final String TOPIC = combinedConfig.getString("kafka.topic");
+    public static final String EXECUTABLE = appConfig.getString("app.executable");
+    public static final String ENVIRONMENT = appConfig.getString("app.environment", "local");
+    public static final String IMAGE = appConfig.getString("app.image");
+    public static final String TOPIC = appConfig.getString("kafka.topic");
 
+    private static List<Properties> getWithPrefix(Configuration config, String prefix) {
+        throw new NotImplementedException();
+    }
     public static Properties getConsumerProps() {
         Properties props = new Properties();
 
-        combinedConfig.getKeys().forEachRemaining(key -> {
+        appConfig.getKeys().forEachRemaining(key -> {
             if (key.startsWith("kafka.")) {
-                props.put(key.substring(6), combinedConfig.getString(key));
+                props.put(key.substring(6), appConfig.getString(key));
             }
         });
 
         kafkaConfig.getKeys().forEachRemaining(key -> {
             if (key.startsWith("kafka.")) {
-                props.put(key.substring(6), combinedConfig.getString(key));
+                props.put(key.substring(6), kafkaConfig.getString(key));
             }
         });
 
