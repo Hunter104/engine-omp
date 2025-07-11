49d5ea604645b9afaa09bcc00dd6493f4a1a99c5
Hunter104 <000plagueinc@gmail.com>
2025-07-11 16:53:14 -0300

    Permite configuração por variável de ambiente

--- kafka-apps/common/src/main/java/com/pspd/ConfigLoader.java
+++ kafka-apps/common/src/main/java/com/pspd/ConfigLoader.java
@@ -3,66 +3,31 @@ package com.pspd;
 import org.apache.commons.configuration2.*;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
-import org.apache.commons.configuration2.tree.OverrideCombiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 public class ConfigLoader {
     private static final Logger logger = LoggerFactory.getLogger(ConfigLoader.class);
     private static final CombinedConfiguration appConfig;
-    private static final PropertiesConfiguration kafkaConfig;
-    private static final String MAIN_CONFIG_FILE = "app.properties";
-    private static final String KAFKA_CONFIG_FILE = "kafka.properties";
 
     static {
         try {
-            appConfig = new CombinedConfiguration(new OverrideCombiner());
-            Configurations configs = new Configurations();
+            appConfig = (new Configurations()).combinedBuilder(new File("config.xml")).getConfiguration();
             appConfig.setThrowExceptionOnMissing(true);
-
-            kafkaConfig = configs.properties(ConfigLoader.class.getClassLoader().getResource(KAFKA_CONFIG_FILE));
-            kafkaConfig.setThrowExceptionOnMissing(true);
-            logger.info("Loaded kafka config from classpath {}", kafkaConfig);
-
-
-            File externalFile = new File(MAIN_CONFIG_FILE);
-            if (externalFile.exists()) {
-                appConfig.addConfiguration(configs.properties(externalFile));
-                logger.info("Loaded external config from {}", externalFile.getAbsolutePath());
-            } else {
-                logger.warn("External config file not found: {}", externalFile.getAbsolutePath());
-            }
-
-            PropertiesConfiguration defaultConfig = configs.properties(
-                    ConfigLoader.class.getClassLoader().getResource(MAIN_CONFIG_FILE)
-            );
-            logger.info("Loaded default config from classpath");
-            appConfig.addConfiguration(defaultConfig);
-
+            logger.info("Loaded config.");
         } catch (ConfigurationException e) {
             throw new RuntimeException("Failed to load config", e);
         }
     }
 
-    private static Map<String, String> filterAndRemovePrefix(Configuration config, String prefix) {
-        return ConfigurationConverter.getMap(config).entrySet().stream()
-                .filter(entry -> ((String) entry.getKey()).startsWith(prefix))
-                .collect(Collectors.toMap(
-                        entry -> ((String) entry.getKey()).substring(prefix.length()+1),
-                        entry -> String.valueOf(entry.getValue())
-                ));
-    }
-
     public static Properties getConsumerProps() {
         Properties props = new Properties();
 
-        props.putAll(filterAndRemovePrefix(appConfig, "kafka"));
-        props.putAll(filterAndRemovePrefix(kafkaConfig, "kafka"));
+        appConfig.getKeys().forEachRemaining(key -> {
+            if (key.startsWith("kafka")) props.put(key.substring("kafka.".length()), appConfig.getProperty(key));});
 
         return props;
     }
